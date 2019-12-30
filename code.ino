134e77f2cd30e7cf85b52602c13e3b200e44d9b4
jerryjaimon <45946840+jerryjaimon@users.noreply.github.com>
2019-12-30 15:22:31 +0400

    Update code.ino

--- code.ino
+++ code.ino
@@ -23,7 +23,41 @@ int mode(int trigger, int echo)
   return dist;
 }
 void loop() {
-distM=mode(t1,e1);
-delay(500);
-Serial.println(distM);
+  int count=0;
+  distM=mode(t1,e1);
+  //Middle Ultrasonic sensor
+  if (distM<20 && distM>1)
+    {
+    
+      while(1){
+      Serial.println("Change window");
+      delay(500);
+      distM=mode(t1,e1);
+      if(distM<1 || distM>20)
+        {Serial.println("Enter");
+        break;}
+      else  
+        continue;}
+    }
+
+  else if(distM>20 && distM <30)
+    {
+      for(int i=0;i<4;++i)
+       {
+        count+=1;
+        if(count>=3)
+          {Serial.println("Close");
+          count=0;}
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(1000);
+        distM=mode(t1,e1);
+        if(distM<20 || distM>30)
+          {Serial.println("Broke");
+          digitalWrite(LED_BUILTIN,LOW);
+          break;
+          }
+        digitalWrite(LED_BUILTIN,LOW);
+       }
+     }
+    delay(1000);
 }
