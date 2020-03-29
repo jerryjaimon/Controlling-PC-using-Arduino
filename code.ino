0b4f1b44c9a88e27fdb10faf5d6fb40a8ce649e3
jerryjaimon <45946840+jerryjaimon@users.noreply.github.com>
2020-03-29 14:19:18 +0530

    Completed code for right sensor

--- code.ino
+++ code.ino
@@ -1,63 +1,76 @@
-const int t1 = 13;
-const int e1 = 12;
-int t;
-float distM;
+
+const int trigger2 = 7; //Trigger pin of 2nd Sesnor
+const int echo2 = 6;//Echo pin of 2nd Sesnor
+long time_taken;
+int dist,distL,distR,l=0;
+ 
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-
+    Serial.begin(9600); 
+    pinMode(trigger2, OUTPUT); 
+    pinMode(echo2, INPUT); 
 }
 
-int mode(int trigger, int echo)
+/*###Function to calculate distance###*/
+void calculate_distance(int trigger, int echo)
 {
-  int dist=0;
-  digitalWrite(trigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(2);
-  digitalWrite(trigger, LOW);
-  t=pulseIn(echo,HIGH); 
-  dist=t*0.034/2;
-  if(dist>50)
-    dist=50;
-  return dist;
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+
+    time_taken = pulseIn(echo, HIGH);
+    dist= time_taken*0.034/2;
+    if (dist>60)
+    dist = 60;
 }
-void loop() {
-  int count=0;
-  distM=mode(t1,e1);
-  //Middle Ultrasonic sensor
-  if (distM<20 && distM>1)
+
+void loop() { 
+    calculate_distance(trigger2,echo2);
+    distR =dist;
+
+    //Lock Right - Control Mode
+    if (distR>=1 && distR<=25)
     {
-    
-      while(1){
-      Serial.println("Change window");
-      delay(500);
-      distM=mode(t1,e1);
-      if(distM<1 || distM>20)
-        {Serial.println("Enter");
-        break;}
-      else  
-        continue;}
+        delay(500); //Hand Hold Time
+        calculate_distance(trigger2,echo2);
+        distR =dist;
+      if (distR>=1 && distR<=37)
+      {
+        int temp=dist;
+        delay(200);
+        //Serial.println("Right Locked");
+        
+        while(distR<=40)
+        {
+          delay(50);
+          calculate_distance(trigger2,echo2);
+          distR =dist;
+          if(distR<=3)
+             {
+              Serial.println("4");
+              delay(200);
+            }  
+          else if((temp+6)<distR)                // this condition becomes true if we moves our hand away from the right sensor (**but in front of it ). here " temp+6 " is for calibration.
+            {
+              if(distR>40)
+                {
+                  continue;
+                }
+              Serial.println("6");
+              delay(400);
+            }
+          else if((temp-2)>distR)           // this condition becomes true if we moves our hand closer to the right sensor.
+            {
+              Serial.println("5");                // send "up" serially.
+              delay(400);
+            }
+          }
+      }
+      
+      else if( distR>30)
+          {Serial.println("2");}  
     }
 
-  else if(distM>20 && distM <30)
-    {
-      for(int i=0;i<4;++i)
-       {
-        count+=1;
-        if(count>=3)
-          {Serial.println("Close");
-          count=0;}
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(1000);
-        distM=mode(t1,e1);
-        if(distM<20 || distM>30)
-          {Serial.println("Broke");
-          digitalWrite(LED_BUILTIN,LOW);
-          break;
-          }
-        digitalWrite(LED_BUILTIN,LOW);
-       }
-     }
-    delay(1000);
+    delay(200);
 }
